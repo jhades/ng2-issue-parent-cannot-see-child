d434afb203dd075e141f8108887a919bb3010f8f
JHades <jhades.dev@gmail.com>
2015-11-01 18:14:22 +0100

    ongoing

--- gulpfile.js
+++ gulpfile.js
@@ -24,13 +24,13 @@ gulp.task('ts2js', function () {
     return tsResult.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('play', ['ts2js'], function () {
+gulp.task('default', ['ts2js'], function () {
     var http = require('http');
     var connect = require('connect');
     var serveStatic = require('serve-static');
     var open = require('open');
 
-    var port = 9000, app;
+    var port = 9001, app;
 
     gulp.watch(PATHS.src, ['ts2js']);
 
